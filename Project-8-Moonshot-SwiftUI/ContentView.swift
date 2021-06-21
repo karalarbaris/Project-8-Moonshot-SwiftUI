6d8f9cd31636e6284980fe7cbebfc211f6ca7e88
Baris <karalarbaris@gmail.com>
2021-06-21 18:29:47 +0200

    Initial Commit

---
+++ Project-8-Moonshot-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-8-Moonshot-SwiftUI
+//
+//  Created by Baris Karalar on 21.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-8-Moonshot-SwiftUI/Project_8_Moonshot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_8_Moonshot_SwiftUIApp.swift
+//  Project-8-Moonshot-SwiftUI
+//
+//  Created by Baris Karalar on 21.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_8_Moonshot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
