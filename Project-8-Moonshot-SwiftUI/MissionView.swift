614e027a267955b56f7f013676f196c99503250a
Baris <karalarbaris@gmail.com>
2021-06-23 18:31:20 +0200

    Showing mission details with ScrollView and GeometryReader

---
+++ Project-8-Moonshot-SwiftUI/MissionView.swift
@@ -0,0 +1,47 @@
+//
+//  MissionView.swift
+//  Project-8-Moonshot-SwiftUI
+//
+//  Created by Baris Karalar on 23.06.2021.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    
+    let mission: Mission
+    
+    var body: some View {
+
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    
+                    Text(mission.description)
+                        .padding()
+                    
+                    Spacer(minLength: 25)
+                    
+                }
+            }
+        }
+        .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
+    }
+    
+    
+}
+
+
+struct MissionView_Previews: PreviewProvider {
+    
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0])
+    }
+}
